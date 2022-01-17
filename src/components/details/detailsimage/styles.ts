9a3e3988e9ac0353b243d83f939324a7cf804176
THIAGO MOYSES <thiagodasilvamoysesfr@gmail.com>
2022-01-16 22:43:57 -0300

    Creating again the styles because of a problem on the computer

---
+++ src/components/details/detailsimage/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const DetailsImageContainer = styled.div`
+
+    width: 40%;
+    display: table-cell;
+    vertical-align: middle;
+    padding: 2rem 2rem 2rem 4rem;
+`
+
+export default DetailsImageContainer;
