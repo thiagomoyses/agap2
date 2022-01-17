c4d4d5240b7a28be171c8e86cb525d275cb959bd
THIAGO MOYSES <thiagodasilvamoysesfr@gmail.com>
2022-01-16 21:15:05 -0300

    Creating the component to display the Episode list

--- src/components/home/movieinfo/MovieEpisodeList.tsx
+++ src/components/home/movieinfo/MovieEpisodeList.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppState } from "redux/store";
+import { Link } from "react-router-dom";
 
 import { getMovieEpisodes } from "redux/effects/home/movieepisodelist/MovieEpisodeListEffect";
 import { IMovieEpisodeList } from "redux/interfaces/home/movieepisodelist/MovieEpisodeList";
@@ -17,10 +18,15 @@ const MovieEpisodes = () => {
     const movieEpisodes = useSelector((state: AppState) => state.episodeList);
 
     const movieEpisodesItem = movieEpisodes.episodes.map((episode: IMovieEpisodeList) => (
-        <div key={ episode.id }>
-            <h1>{ episode.name }</h1>
-            <br/>
-        </div>
+        <Link
+            to={`/details/${ episode.id }`}
+            key={ episode.id }
+        >
+            <div>
+                <h1>{ episode.name }</h1>
+                <br/>
+            </div>
+        </Link>
     ))
 
     return (
